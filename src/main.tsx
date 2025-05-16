ffd02f837cefe53bb5541a4555f6194344ef1502
Lucian <cojlucian89@gmail.com>
2025-05-16 22:50:45 +0300

    Testare pentru header

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Header from './header/Header'; // Importăm direct fișierul Header.tsx
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-  </StrictMode>,
-)
+    <Header 
+      title="Site-ul Meu" 
+      logo="/assets/background.svg"
+    />
+    <App />
+  </StrictMode>
+);
