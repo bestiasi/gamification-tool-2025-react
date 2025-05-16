1809894ae1c019531fd267a6b75b77d038bc9487
Lucian <cojlucian89@gmail.com>
2025-05-16 23:11:43 +0300

    header var1

---
+++ src/Header/index.ts
@@ -0,0 +1,4 @@
+import Header from './Header';
+
+// Exportăm componenta Header
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Header from './Header/Header'; // Corectăm calea pentru Header
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-  </StrictMode>,
-)
+    <Header 
+      title="Site-ul Meu"
+      logo="/assets/background.svg"
+    />
+    <App />
+  </StrictMode>
+);
